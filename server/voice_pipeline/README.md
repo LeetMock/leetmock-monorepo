cbf0df5163212301ecd794f7582bf9b9114a3e69
Brian Yin <byin@roblox.com>
2024-08-12 21:59:14 -0700

    partial update

--- server/voice_pipeline/README.md
+++
@@ -1,5 +0,0 @@
-# Voice Pipeline
-
-View the latest architecture at [here](https://lucid.app/lucidchart/44141db5-9cf7-40df-a2ab-d239a31d9044/edit?invitationId=inv_8b2894b3-a0a5-490c-a3a4-e12a05ed1470)
-
-Updates to the architecture will be made in the Lucidchart document.
---
+++ voice_pipeline/voice_pipeline/custom_types.py
@@ -0,0 +1,84 @@
+from collections import defaultdict
+import threading
+from typing import DefaultDict, List, Literal, Optional, Union
+from pydantic import BaseModel, ConfigDict, Field
+import websocket
+
+from voice_pipeline.voice_server import AudioStreamBuffer, WordsStreamBuffer
+
+
+class AudioRequest(BaseModel):
+    data: bytes
+    user_id: int
+    session_id: str
+
+class EventRequest(BaseModel):
+    event_type: Optional[str] = None
+    user_id: int
+    session_id: str
+    data: Optional[dict] = None
+
+class RegisterInterviewRequest(BaseModel):
+    user_id: int
+    question_id: int
+    session_period: int
+
+
+class EndInterviewRequest(BaseModel):
+    user_id: int
+    session_id: str
+
+
+class EventResponse(BaseModel):
+    success: bool
+    error: Optional[str] = None
+    data: Optional[dict] = None
+
+
+class LLMResponseResponse(BaseModel):
+    response_type: Literal["response"] = "response"
+    response_id: int
+    content: str
+    content_complete: bool
+    end_call: Optional[bool] = False
+    transfer_number: Optional[str] = None
+
+
+class WordTimestamp(BaseModel):
+    word: str
+    start: float
+    end: float
+
+
+class Utterance(BaseModel):
+    role: Literal["agent", "user", "system"]
+    content: str
+    words: List[WordTimestamp]
+
+
+class ResponseRequiredRequest(BaseModel):
+    interaction_type: Literal["reminder_required", "response_required"]
+    response_id: int
+    transcript: List[Utterance]
+
+
+class LLMPingPongResponse(BaseModel):
+    response_type: Literal["ping_pong"] = "ping_pong"
+    timestamp: int
+
+
+class UserConnection(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    session_id: str
+    user_id: int
+    ws: websocket.WebSocketApp
+    latest_response_id: int
+    words_map: DefaultDict[str, List[str]] = Field(
+        default_factory=lambda: defaultdict(list)
+    )
+    utterance_list: List[Utterance] = Field(default_factory=list)
+    audio_buffer: AudioStreamBuffer = Field(default_factory=AudioStreamBuffer)
+    word_buffer: WordsStreamBuffer = Field(default_factory=WordsStreamBuffer)
+    voice: str = "nova"
+LLMResponse = Union[LLMResponseResponse, LLMPingPongResponse]
