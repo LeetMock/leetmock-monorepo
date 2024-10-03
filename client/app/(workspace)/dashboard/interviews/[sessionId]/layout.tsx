f96c091e8279bcc0c73b1088745e26fbaacf606c
Brian Yin <byin@roblox.com>
2024-10-02 20:53:41 -0700

    Update layout.tsx

--- client/app/(workspace)/dashboard/interviews/[sessionId]/layout.tsx
+++ client/app/(workspace)/dashboard/interviews/[sessionId]/layout.tsx
@@ -45,7 +45,6 @@ export default function InterviewLayout({
 }
 
 function onDeviceFailure(error?: MediaDeviceFailure) {
-  console.error(error);
   alert(
     "Error acquiring camera or microphone permissions. Please make sure you grant the necessary permissions in your browser and reload the tab"
   );
