19d0d9d24830b2ea2b66db49ffbfd731149a3051
Brian Yin <byin@roblox.com>
2024-10-02 21:12:53 -0700

    update

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,14 +3,14 @@ import PricingPage from "./pricing/page";
 import { FullScreenSection } from "@/components/FullScreenSection";
 
 export default function Home() {
-    return (
-        <div className="home-container h-screen overflow-y-auto snap-y snap-mandatory">
-            <FullScreenSection>
-                <Hero />
-            </FullScreenSection>
-            <FullScreenSection>
-                <PricingPage />
-            </FullScreenSection>
-        </div>
-    );
+  return (
+    <div className="h-screen overflow-y-auto snap-y snap-mandatory">
+      <FullScreenSection>
+        <Hero />
+      </FullScreenSection>
+      <FullScreenSection>
+        <PricingPage />
+      </FullScreenSection>
+    </div>
+  );
 }
