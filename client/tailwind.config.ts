a1e09ab09fcc664314c5093315778e612a367211
Brian Yin <byin@roblox.com>
2024-11-07 01:45:21 -0800

    Update tailwind.config.ts

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -12,7 +12,7 @@ const config = {
   prefix: "",
   theme: {
     container: {
-      center: "true",
+      center: true,
       padding: "2rem",
       screens: {
         "2xl": "1400px",
@@ -167,22 +167,6 @@ const config = {
             filter: "blur(0)",
           },
         },
-        "accordion-down": {
-          from: {
-            height: "0",
-          },
-          to: {
-            height: "var(--radix-accordion-content-height)",
-          },
-        },
-        "accordion-up": {
-          from: {
-            height: "var(--radix-accordion-content-height)",
-          },
-          to: {
-            height: "0",
-          },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
@@ -191,8 +175,6 @@ const config = {
         fadeIn: "fadeIn 0.5s ease-in",
         "marquee-horizontal": "marquee-x var(--duration) infinite linear",
         "marquee-vertical": "marquee-y var(--duration) linear infinite",
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
     screens: {
