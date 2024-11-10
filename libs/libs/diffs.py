81ba5691f89653e2fcec6bcead8bf37e8fd4937d
Brian Yin <byin@roblox.com>
2024-11-09 18:19:01 -0800

    Update diffs.py

--- libs/libs/diffs.py
+++ libs/libs/diffs.py
@@ -2,7 +2,9 @@ import difflib
 
 
 def get_unified_diff(before: str, after: str, n_context_lines: int = 3) -> str:
-    diff = difflib.unified_diff(
-        before.splitlines(), after.splitlines(), n=n_context_lines, lineterm=""
+    diff = list(
+        difflib.unified_diff(
+            before.splitlines(), after.splitlines(), n=n_context_lines, lineterm=""
+        )
     )
-    return "\n".join(diff)
+    return "\n".join(diff[2:])
