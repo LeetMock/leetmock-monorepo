c5856383077d768310fe5f75daf99570592e50dd
Brian Yin <byin@roblox.com>
2024-11-10 16:35:56 -0800

    delete client for better merging

--- convex-client/convex_client/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from convex_client.api.action_api import ActionApi
-from convex_client.api.mutation_api import MutationApi
-from convex_client.api.query_api import QueryApi
-
