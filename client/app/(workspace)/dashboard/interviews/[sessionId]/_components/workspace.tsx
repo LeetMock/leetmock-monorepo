7eaec30ed9eb3e3b563283a608eb0fab2a5645bc
Brian Yin <byin@roblox.com>
2024-11-27 01:17:51 -0800

    update

--- client/app/(workspace)/dashboard/interviews/[sessionId]/_components/workspace.tsx
+++ client/app/(workspace)/dashboard/interviews/[sessionId]/_components/workspace.tsx
@@ -4,47 +4,56 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/comp
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
 import { ChevronLeft } from "lucide-react";
+import { HTMLAttributes } from "react";
 import { TimerCountdown } from "./timer-countdown";
 
 export const Workspace: React.FC<{ sessionId: Id<"sessions"> }> = ({ sessionId }) => {
   return (
-    <div className="bg-background h-screen w-full flex">
-      <div className="w-56 bg-background flex flex-col h-full">
-        <div
+    <div className="bg-background h-screen w-full flex flex-col">
+      {/* Header */}
+      <div className="flex w-full border-b h-12">
+        {/* Logo */}
+        <Logo
+          showText={false}
           className={cn(
-            "w-full flex items-center justify-between pl-4 pr-2 h-14",
-            "cursor-pointer group"
+            "items-center justify-center cursor-pointer w-12 hover:bg-accent",
+            "transition-all duration-200"
           )}
-        >
-          <Logo />
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  size="icon"
-                  variant="ghost"
-                  className="transition-all duration-200 opacity-0 group-hover:opacity-100"
-                >
-                  <ChevronLeft className="w-4 h-4 text-primary" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent>Collapse</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </div>
-        <div className="w-full h-full flex flex-col justify-between">
+        />
+      </div>
+      {/* Main */}
+      <div className="flex justify-center flex-1">
+        {/* Timeline */}
+        <div className="w-56 h-full flex flex-col justify-between border-r relative group">
           Side
           <div className="w-full p-2 h-[3.2rem]">
             <TimerCountdown timeLeft={1000} className="h-full w-full" />
           </div>
+          <CollapseButton className="absolute top-2 right-2 opacity-0 group-hover:opacity-100" />
         </div>
-      </div>
-      <div className="flex flex-col justify-center items-center flex-1 bg-accent">
-        <div className={cn("w-full h-14 flex items-center justify-center px-2")}></div>
-        <div className="w-full h-full flex justify-center items-center p-2 pt-0">
-          <div className="w-full h-full bg-background rounded-sm shadow-sm"></div>
-        </div>
+        {/* Content */}
+        <div className="h-full flex-1 justify-center items-center p-2 pt-0">Content</div>
       </div>
     </div>
   );
-}; // Dependencies: pnpm install lucide-react
+};
+
+const CollapseButton: React.FC<HTMLAttributes<HTMLButtonElement>> = ({ className, ...props }) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            size="icon"
+            variant="ghost"
+            className={cn("transition-all duration-200", className)}
+            {...props}
+          >
+            <ChevronLeft className="w-4 h-4 text-primary" />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>Collapse</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
