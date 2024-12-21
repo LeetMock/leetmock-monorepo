69d4f411553ba3f95736bc52200d445e90da31ae
Boquan (Brian) Yin <57741529+Toubat@users.noreply.github.com>
2024-12-21 16:41:34 +0800

    update

--- agent-server/agent_server/livekit/streams.py
+++ agent-server/agent_server/livekit/streams.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
-import asyncio
 import uuid
-from typing import AsyncIterator, List
+from typing import AsyncIterator, Literal, Self, Union
 
 from agent_server.utils.logger import get_logger
 from livekit.agents import llm
+from livekit.agents.llm import ToolChoice, function_context
+from livekit.agents.types import DEFAULT_API_CONNECT_OPTIONS, APIConnectOptions
 
 logger = get_logger(__name__)
 
@@ -16,30 +17,20 @@ class NoopLLM(llm.LLM):
         self,
         *,
         chat_ctx: llm.ChatContext,
+        conn_options: APIConnectOptions = DEFAULT_API_CONNECT_OPTIONS,
         fnc_ctx: llm.FunctionContext | None = None,
         temperature: float | None = None,
-        n: int | None = None,
+        n: int | None = 1,
         parallel_tool_calls: bool | None = None,
+        tool_choice: (
+            Union[ToolChoice, Literal["auto", "required", "none"]] | None
+        ) = None,
     ) -> llm.LLMStream:
-        return NoopStream(chat_ctx=chat_ctx)
-
-
-class IntermittentEchoLLM(llm.LLM):
-
-    def __init__(self, stream: List[str | float]):
-        super().__init__()
-        self._stream = stream
-
-    def chat(
-        self,
-        *,
-        chat_ctx: llm.ChatContext,
-        fnc_ctx: llm.FunctionContext | None = None,
-        temperature: float | None = None,
-        n: int | None = None,
-        parallel_tool_calls: bool | None = None,
-    ) -> llm.LLMStream:
-        return IntermittentEchoStream(stream=self._stream, chat_ctx=chat_ctx)
+        return NoopStream(
+            chat_ctx=chat_ctx,
+            fnc_ctx=fnc_ctx,
+            conn_options=conn_options,
+        )
 
 
 class NoopStream(llm.LLMStream):
@@ -49,12 +40,23 @@ class NoopStream(llm.LLMStream):
         self,
         *,
         chat_ctx: llm.ChatContext,
+        fnc_ctx: function_context.FunctionContext | None,
+        conn_options: APIConnectOptions = DEFAULT_API_CONNECT_OPTIONS,
     ):
-        super().__init__(llm=NoopLLM(), chat_ctx=chat_ctx, fnc_ctx=None)
+        super().__init__(
+            llm=NoopLLM(),
+            chat_ctx=chat_ctx,
+            fnc_ctx=fnc_ctx,
+            conn_options=conn_options,
+        )
         self._stream = self._create_noop_stream()
         self._request_id = str(uuid.uuid4())
 
-    async def _main_task(self):
+    @classmethod
+    def from_chat_ctx(cls, chat_ctx: llm.ChatContext) -> Self:
+        return cls(chat_ctx=chat_ctx)
+
+    async def _run(self):
         pass
 
     async def _create_noop_stream(self) -> AsyncIterator[llm.ChatChunk]:
@@ -67,12 +69,30 @@ class NoopStream(llm.LLMStream):
 class EchoStream(llm.LLMStream):
     """Echoes the text stream back to the user."""
 
-    def __init__(self, *, text_stream: AsyncIterator[str], chat_ctx: llm.ChatContext):
-        super().__init__(llm=NoopLLM(), chat_ctx=chat_ctx, fnc_ctx=None)
+    def __init__(
+        self,
+        text_stream: AsyncIterator[str],
+        *,
+        chat_ctx: llm.ChatContext,
+        fnc_ctx: function_context.FunctionContext | None,
+        conn_options: APIConnectOptions = DEFAULT_API_CONNECT_OPTIONS,
+    ):
+        super().__init__(
+            llm=NoopLLM(),
+            chat_ctx=chat_ctx,
+            fnc_ctx=fnc_ctx,
+            conn_options=conn_options,
+        )
         self._chunk_stream = self._create_message_chunk_stream(text_stream)
         self._request_id = str(uuid.uuid4())
 
-    async def _main_task(self):
+    @classmethod
+    def from_chat_ctx(
+        cls, text_stream: AsyncIterator[str], chat_ctx: llm.ChatContext
+    ) -> Self:
+        return cls(text_stream=text_stream, chat_ctx=chat_ctx)
+
+    async def _run(self):
         pass
 
     async def _create_message_chunk_stream(self, text_stream: AsyncIterator[str]):
@@ -81,34 +101,6 @@ class EchoStream(llm.LLMStream):
 
     async def __anext__(self) -> llm.ChatChunk:
         return await anext(self._chunk_stream)
-
-
-class IntermittentEchoStream(llm.LLMStream):
-    """Echoes the text stream back to the user intermittently."""
-
-    def __init__(self, stream: List[str | float], chat_ctx: llm.ChatContext):
-        super().__init__(llm=NoopLLM(), chat_ctx=chat_ctx, fnc_ctx=None)
-        self._intermittent_stream = self._create_intermittent_stream(stream)
-        self._request_id = str(uuid.uuid4())
-
-    async def _main_task(self):
-        pass
-
-    async def _create_intermittent_stream(self, stream: List[str | float]):
-        logger.info(f"Intermittent stream: {stream}")
-
-        for item in stream:
-            if isinstance(item, str):
-                yield create_llm_chunk(self._request_id, item)
-            elif isinstance(item, float) or isinstance(item, int):
-                await asyncio.sleep(item)
-            else:
-                raise ValueError(f"Invalid item type: {type(item)}")
-
-        logger.info("Intermittent stream finished")
-
-    async def __anext__(self) -> llm.ChatChunk:
-        return await anext(self._intermittent_stream)
 
 
 def create_llm_chunk(request_id: str, content: str) -> llm.ChatChunk:
