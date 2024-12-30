1716948b593ab7a8916d285bed2b73014fd2f249
Boquan (Brian) Yin <57741529+Toubat@users.noreply.github.com>
2024-12-30 17:43:37 +0800

    Update layout.tsx

--- client/app/admin/layout.tsx
+++ client/app/admin/layout.tsx
@@ -1,8 +1,22 @@
 "use client";
 
 import { DashboardHeader } from "@/app/(workspace)/dashboard/_components/dashboard-header";
+import { useUserProfile } from "@/hooks/use-user-profile";
+import { isDefined } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
-export default function QuestionsLayout({ children }: { children: React.ReactNode }) {
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  const { userProfile, isLoaded } = useUserProfile();
+  const router = useRouter();
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isDefined(userProfile) || userProfile.role !== "admin") {
+    router.push("/dashboard");
+  }
+
   return (
     <div className="flex flex-col h-full">
       <DashboardHeader className="px-6 border-b sticky top-0 z-50 bg-background" />
